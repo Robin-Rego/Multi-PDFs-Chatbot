4a1817ade03e2159889b6c1e0d7573e75fc84927
Robin Rego <98087285+Robin-Rego@users.noreply.github.com>
2025-11-07 15:52:06 +0530

    Add files via upload

---
+++ chatapp.py
@@ -0,0 +1,112 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+# from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
+# os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=50000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = prompt | model | StrOutputParser()
+
+    return chain
+
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+    
+    context = "\n".join(doc.page_content for doc in docs)
+    response = chain.invoke({"context": context, "question": user_question})
+
+    print(response)
+    st.write("Reply: ", response)
+
+
+
+
+def main():
+    st.set_page_config("Multi PDF Chatbot", page_icon = ":scroll:")
+    st.header("Multi-PDF's 📚 - Chat Agent 🤖 ")
+
+    user_question = st.text_input("Ask a Question from the PDF Files uploaded .. ✍️📝")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+
+        st.title("📁 PDF File's Section")
+        pdf_docs = st.file_uploader("Upload your PDF Files & \n Click on the Submit & Process Button ", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."): # user friendly message.
+                raw_text = get_pdf_text(pdf_docs) # get the pdf text
+                text_chunks = get_text_chunks(raw_text) # get the text chunks
+                get_vector_store(text_chunks) # create vector store
+                st.success("Done")
+        
+        st.write("---")
+        st.write("AI App created by @ Robin Rego")  # add this line to display the image
+
+
+    st.markdown(
+        """
+        <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #0E1117; padding: 15px; text-align: center;">
+            © <a href="https://github.com/Robin-Rego" target="_blank">Robin Rego</a> | Made with ❤️
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+if __name__ == "__main__":
+    main()
